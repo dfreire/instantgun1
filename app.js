ef567d82644e6bc494d6b4d8029fe105e47146a9
dfreire <dario.freire@gmail.com>
2018-06-06 13:48:39 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+function getEnv(key) {
+    const value = process.env[key];
+    if (value == null) {
+        console.error(`${key} is not defined`);
+        process.exit(1);
+    }
+    return value;
+}
+
+const PORT = getEnv('PORT');
+const DATA_DIR = getEnv('DATA_DIR');
+
+const http = require('http')
+const Gun = require('gun')
+
+const server = http.createServer();
+
+Gun({
+    web: server,
+    localStorage: false,
+    radisk: true,
+    file: DATA_DIR
+});
+
+server.listen(PORT);
